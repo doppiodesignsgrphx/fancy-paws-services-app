a0665664f2eaff56c7b88af977c3b3d7d1f26745
doppiodesignsgrphx <j.johnson@doppiodesignsgrphx.com>
2025-03-26 15:09:10 -0400

    add starting at text to service card

--- src/ServiceCard.js
+++ src/ServiceCard.js
@@ -5,7 +5,7 @@ export default function ServiceCard({ service, price, description }) {
   return (
     <div className="service-card">
       <h2 className="service-card-title">{service}</h2>
-      <h4>${price}</h4>
+      <h4>starting at ${price}...</h4>
       {/*<p className="service-card-description">{description}</p>*/}
     </div>
   );
